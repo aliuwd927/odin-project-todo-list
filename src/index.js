4d6bc9de5f86b80ad91cb05bbccf68016ee08773
Andy Liu <aliu927@gmail.com>
2021-01-13 00:00:07 -0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import _ from 'lodash';
+
 
  function component() {
-   const element = document.createElement('div');
- 
+   const content = document.querySelector('.content');
 
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
- 
-   return element;
+   content.innerHTML = 'test';
+
+
  }
  
- document.body.appendChild(component());
+ component();

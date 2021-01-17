88eb3638ed7dd5fdbe7513f7f9e2fdea1b4405d9
Andy Liu <aliu927@gmail.com>
2021-01-16 23:14:20 -0800

    add event listener and buttons

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -7,9 +7,17 @@ function projects(){
     const projectContainer = document.createElement('div');
     projectContainer.classList.add('projectContainer');
     
+    const addSubTask = document.createElement('button');
+    addSubTask.classList.add('addSubTask');
+    addSubTask.textContent = '+';
 
+    const removeCurrentProject = document.createElement('button');
+    removeCurrentProject.classList.add('removeCurrentProject');
+    removeCurrentProject.textContent ='-';
 
     leftContent.appendChild(projectContainer);
+    projectContainer.appendChild(addSubTask);
+    projectContainer.appendChild(removeCurrentProject);
 }
 
 export default projects;

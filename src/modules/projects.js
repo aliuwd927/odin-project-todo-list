a13a31f9ba626a4f455944ddf9206aa0ef7fabd5
Andy Liu <aliu927@gmail.com>
2021-01-23 15:30:45 -0800

    added array to projects , function constructor

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -5,22 +5,30 @@ const projects = () =>{
     const submitButton = document.querySelector('.addToDoProjects');
 
     const array = [];
+    
+    
+    class TestOne {
+        constructor(title,description){
+            this.title = title;
+            this.description = description
+        }
+
+    };
+
 
     submitButton.addEventListener('click', () =>{
 
     const todoInputTitle = document.querySelector('.inputTitle').value;
     const todoInputDescription = document.querySelector('.inputDescription').value;
     //console.log(todoInputTitle,todoInputDescription);
-    
-    const testOne = (title,description) =>{
-        array.push({title,description});
-        console.log(array);
-    };
 
-    testOne(todoInputTitle,todoInputDescription);
-    
+    //testOne(todoInputTitle,todoInputDescription);
 
+    array.push(new TestOne(todoInputTitle,todoInputDescription));
+    console.log(array);
 
+    document.querySelector('.inputTitle').value = '';
+    document.querySelector('.inputDescription').value = '';
 
     const leftContent = document.querySelector('.leftContent');
 

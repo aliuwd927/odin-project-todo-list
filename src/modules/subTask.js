5d40f5c7fd215585d44dd6f902ed639985602a60
Andy Liu <aliu927@gmail.com>
2021-01-28 00:52:19 -0800

    commit

--- src/modules/subTask.js
+++ src/modules/subTask.js
@@ -3,7 +3,26 @@ const subTask = () => {
 
     const rightContent = document.querySelector('.rightContent');
 
-    rightContent.textContent = 'test';
+    //Append subTaskTop/Btm Container under rightContent
+    const subTaskTopContainer = document.createElement('div');
+    const subTaskBtmContainer = document.createElement('div');
+
+    //Render under subTaskTopContainer
+    const task0 = document.createElement('input');
+    const task1 = document.createElement('input');
+    const task2 = document.createElement('input');
+    const task3 = document.createElement('input');
+
+    //classList
+    subTaskTopContainer.classList.add('subTaskTopContainer');
+
+    //Render under subTaskBtmContainer
+    subTaskTopContainer.appendChild(task3);
+    subTaskTopContainer.appendChild(task2);
+    subTaskTopContainer.appendChild(task1);
+    subTaskTopContainer.appendChild(task0);
+    rightContent.appendChild(subTaskBtmContainer);
+    rightContent.appendChild(subTaskTopContainer);
 
 };
 

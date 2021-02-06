04db5dcff4a5a866e4d0502fb1851099f7d4a050
Andy Liu <aliu927@gmail.com>
2021-02-06 15:56:09 -0800

    added toggle box to show or hide subTask items.

--- src/modules/displayProjects.js
+++ src/modules/displayProjects.js
@@ -16,23 +16,33 @@ const displayProjects = (renderArrayItems) => {
     //render under leftBtmContainer
     const projectContainer = document.createElement('div');
 
-    //render under projectContainer
-    const projectTextContainer = document.createElement('div');
+    //render checkToggleContainer
+    const checkToggleContainer = document.createElement('div');
+    const checkToggle = document.createElement('input');
 
+    //set Attribute
+    checkToggle.setAttribute('type', 'checkbox');
+
+    //render projectTextContainer
+    const projectTextContainer = document.createElement('div');
     const projectAddedTitle = document.createElement('div');
     const projectAddedDescription = document.createElement('div');
     const projectAddedDate = document.createElement('div');
     const projectAddedPrority = document.createElement('div');
+    //render taskBtnContainer
+    const taskBtnContainer = document.createElement('div');
     const addSubTask = document.createElement('button');
     const removeCurrentProject = document.createElement('button');
 
     //classList
+    checkToggleContainer.classList.add('checkToggleContainer');
     projectContainer.classList.add('projectContainer');
     projectTextContainer.classList.add('projectTextContainer');
     projectAddedTitle.classList.add('projectAddedTitle');
     projectAddedDescription.classList.add('projectAddedDescription');
     projectAddedDate.classList.add('projectAddedDate');
     projectAddedPrority.classList.add('projectAddedPrority');
+    taskBtnContainer.classList.add('taskBtnContainer');
     addSubTask.classList.add('addSubTask');
     removeCurrentProject.classList.add('removeCurrentProject');
 
@@ -45,13 +55,16 @@ const displayProjects = (renderArrayItems) => {
     removeCurrentProject.textContent ='-';
 
     //projectContainer.appendChild(projectAdded);
+    projectContainer.appendChild(checkToggleContainer);
+    checkToggleContainer.appendChild(checkToggle);
     projectTextContainer.appendChild(projectAddedTitle);
     projectTextContainer.appendChild(projectAddedDescription);
     projectTextContainer.appendChild(projectAddedDate);
     projectTextContainer.appendChild(projectAddedPrority);
-    projectContainer.appendChild(addSubTask);
-    projectContainer.appendChild(removeCurrentProject);
     projectContainer.appendChild(projectTextContainer);
+    taskBtnContainer.appendChild(addSubTask);
+    taskBtnContainer.appendChild(removeCurrentProject);
+    projectContainer.appendChild(taskBtnContainer);
     leftBtmContainer.appendChild(projectContainer);
 
     addSubTask.addEventListener('click', () => {
